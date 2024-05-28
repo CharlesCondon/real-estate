89a1a47a10e4aa6c7f414ca77e44f1cf66145dd2
Charles <charlescon@live.com>
2024-05-28 14:53:07 -0500

    updated data and changed page title

--- src/assets/components/Navbar/Navbar.js
+++ src/assets/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className='navCont'>
-        <h1>Cook County Real Estate MVP</h1>
+        <h1>Prototype</h1>
         {/* <div className='navBtns'>
             <button>button</button>
             <button>button</button>
